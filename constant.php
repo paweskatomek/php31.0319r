200b6bce47c093e9905dcf6af060238de5a42672
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-03-31 14:59:34 +0200

    php basic

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php 
+
+// This creates a case-sensitive constant 
+define("WELCOME", "Zamiana"); 
+echo WELCOME, "\n"; 
+
+// This creates a case-insensitive constant 
+define("HELLO", "Zmiana", true); 
+echo hello; 
+
+?> 
