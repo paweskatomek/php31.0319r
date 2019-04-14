b2d9328df9ed61d3706686213a1ca665a7a4b1c1
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-04-14 09:16:20 +0200

    14.04.19php.sql

--- phonebook.php
+++ phonebook.php
@@ -5,16 +5,20 @@ require ("db.php");
 
 $dbh=conectDb();
 if(!$dbh){
+    mysqli_close($dbh);
     die("No conection" .mysqli_error());
 
 }
+
 if(!mysqli_select_db($dbh,"phone_book")){
+    mysqli_close($dbh);
     die("conect select" .mysqli_error());
 }
 
 $sql_stmt = "SELECT * FROM phonebook";
 $result = mysqli_query($dbh,$sql_stmt);
 if(!$result){
+    mysqli_close($dbh);
     die("unable to execute query");
 }
 
